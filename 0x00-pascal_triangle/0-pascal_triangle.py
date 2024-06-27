7aa3ab0e9476bfa33bdb4082ce18e568b5a45cd4
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-06-27 11:49:52 +0100

    Add solution file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n): #Time complexity: O(n ^ 2) | Space complexity: O(n ^ 2)
+  """Creates a Pascal's Triangle of height n"""
+  if n <= 0:
+    return [] #1
+  elif n == 1:
+    return [[1]] #1
+  else:
+    result = [[1]] #1
+    for i in range(n - 1): #(O(n * n))
+      row = create_rows(result[i]) #n
+      result.append(row) #1
+  return result
+
+
+def create_rows(array): #Time complexity: O(n) | Space complexity: O(n)
+  """Creates new rows of Pascal's Triangle"""
+  result = []
+  for i in range(len(array)): #n
+    if i == 0:
+      result.append(array[i])
+    else:
+      result.append(array[i] + array[i - 1])
+  result.append(array[i])
+
+  return result
