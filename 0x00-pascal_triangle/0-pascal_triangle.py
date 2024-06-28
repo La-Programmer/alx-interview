8e1c290fe79f57ab06522d07a22d4161867b0e78
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-06-28 12:30:49 +0100

    Fix task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """Pascal's Triangle"""
 
-def pascal_triangle(n): #Time complexity: O(n ^ 2) | Space complexity: O(n ^ 2)
-  """Creates a Pascal's Triangle of height n"""
-  if n <= 0:
-    return [] #1
-  elif n == 1:
-    return [[1]] #1
-  else:
-    result = [[1]] #1
-    for i in range(n - 1): #(O(n * n))
-      row = create_rows(result[i]) #n
-      result.append(row) #1
-  return result
 
+def pascal_triangle(n):
+	"""Creates a Pascal's Triangle of height n"""
+	if n <= 0:
+		return []
+	elif n == 1:
+		return [[1]]
+	else:
+		result = [[1]]
+		for i in range(n - 1):
+			row = create_rows(result[i])
+			result.append(row)
+	return result
 
 def create_rows(array): #Time complexity: O(n) | Space complexity: O(n)
-  """Creates new rows of Pascal's Triangle"""
-  result = []
-  for i in range(len(array)): #n
-    if i == 0:
-      result.append(array[i])
-    else:
-      result.append(array[i] + array[i - 1])
-  result.append(array[i])
+	"""Creates new rows of Pascal's Triangle"""
+	result = []
+	for i in range(len(array)): #n
+		if i == 0:
+			result.append(array[i])
+		else:
+			result.append(array[i] + array[i - 1])
+		result.append(array[i])
 
-  return result
+	return result
