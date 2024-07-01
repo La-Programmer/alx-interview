33ee9551402e9598d6ecadbb51f39a68bddcad02
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-01 10:51:40 +0100

    Fix pycodestyle validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,26 +3,27 @@
 
 
 def pascal_triangle(n):
-	"""Creates a Pascal's Triangle of height n"""
-	if n <= 0:
-		return []
-	elif n == 1:
-		return [[1]]
-	else:
-		result = [[1]]
-		for i in range(n - 1):
-			row = create_rows(result[i])
-			result.append(row)
-	return result
+    """Creates a Pascal's Triangle of height n"""
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    else:
+        result = [[1]]
+        for i in range(n - 1):
+            row = create_rows(result[i])
+            result.append(row)
+    return result
 
-def create_rows(array): #Time complexity: O(n) | Space complexity: O(n)
-	"""Creates new rows of Pascal's Triangle"""
-	result = []
-	for i in range(len(array)): #n
-		if i == 0:
-			result.append(array[i])
-		else:
-			result.append(array[i] + array[i - 1])
-	result.append(array[i])
 
-	return result
+def create_rows(array):
+    """Creates new rows of Pascal's Triangle"""
+    result = []
+    for i in range(len(array)):
+        if i == 0:
+            result.append(array[i])
+        else:
+            result.append(array[i] + array[i - 1])
+    result.append(array[i])
+
+    return result
