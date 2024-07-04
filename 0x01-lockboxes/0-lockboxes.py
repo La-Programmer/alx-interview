fd321dea20e1d16cc4fce9a037011dc2e508bab1
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-07-04 09:27:18 +0100

    Solve lockboxes algorithm

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""LockBoxes Algorithm"""
+
+from queue import Queue
+
+
+def canUnlockAll(boxes):
+    """Function to check if all boxes can be unlocked"""
+    all_keys = {x for x in range(len(boxes))}
+    visited_boxes = set()
+    boxes_to_visit = Queue(maxsize=len(boxes))
+    boxes_to_visit.put(0)
+
+    while (boxes_to_visit.empty() is False):
+        box_key = boxes_to_visit.get()
+        box_to_visit = boxes[box_key]
+        for key in box_to_visit:
+            if key not in visited_boxes and key < len(boxes):
+                boxes_to_visit.put(key)
+        visited_boxes.add(box_key)
+    return (visited_boxes & all_keys) == all_keys
