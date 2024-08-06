03cf865d99183ac63d88b0774ab436217e77b4b5
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-06 12:01:35 +0100

    Print out characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,31 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_id = process.argv[2];
-const url = `https://swapi-api.alx-tolls.com/api/films/${movie_id}`;
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tolls.com/api/films/${movieId}`;
 
-function getCharacters(url) {
+function getCharacters (url) {
   request(url, (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
-      console.log("Response: ", response.statusCode);
-      console.log("Response body: ", body);
+      const responseBody = JSON.parse(body);
+      const characters = responseBody.characters;
+      return characters;
     }
-  })
+  });
 }
 
-getCharacters(url);
+const characters = getCharacters(url);
+
+for (const character in characters) {
+  request(character, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const responseBody = JSON.parse(body);
+      const characterName = responseBody.name;
+      console.log(characterName);
+    }
+  });
+}
