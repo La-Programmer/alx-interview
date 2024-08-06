592a8fef98a845ecc006a8e87e5c280d5bff115e
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-06 13:35:03 +0100

    Fix function to use async/await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,28 +4,39 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-function getCharacters (url) {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const responseBody = JSON.parse(body);
-      const characters = responseBody.characters;
-      return characters;
-    }
-  });
+
+async function getCharacters (url) {
+  try {
+    const response = await new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(body);
+        }
+      });
+    });
+    const characters = JSON.parse(response).characters;
+    return characters;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const characters = getCharacters(url);
 
-for (const character in characters) {
-  request(character, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const responseBody = JSON.parse(body);
-      const characterName = responseBody.name;
-      console.log(characterName);
+getCharacters(url)
+.then(
+  characters => {
+    for (const character in characters) {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const responseBody = JSON.parse(body);
+          const characterName = responseBody.name;
+          console.log(characterName);
+        }
+      });
     }
-  });
-}
+  }
+)
