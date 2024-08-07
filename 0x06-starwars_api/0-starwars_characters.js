1c8ad1d53e9e60880b4de4bd04317b5661966eee
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-07 18:40:20 +0100

    Update file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,17 +4,29 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
+const characters = getCharacters(url);
 
-async function getCharacters (url) {
+for (const character in characters) {
+  request(character, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const responseBody = JSON.parse(body);
+      const characterName = responseBody.name;
+      console.log(characterName);
+    }
+  });
+}
+
+function getCharacters (url) {
   try {
-    const response = await new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(body);
-        }
-      });
+    const response = request(url, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return error;
+      } else {
+        return body;
+      }
     });
     const characters = JSON.parse(response).characters;
     return characters;
@@ -22,21 +34,3 @@ async function getCharacters (url) {
     console.log(error);
   }
 }
-
-
-getCharacters(url)
-.then(
-  characters => {
-    for (const character in characters) {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const responseBody = JSON.parse(body);
-          const characterName = responseBody.name;
-          console.log(characterName);
-        }
-      });
-    }
-  }
-)
