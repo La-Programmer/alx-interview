7e9a037c034799dcd702d0d2b4a432165cbf9b6b
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-08-21 14:15:14 +0100

    Fix task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,14 +3,14 @@
 from typing import List
 
 
-def bottom_up_solution(coins: List[int], amount: int) -> int:
-    dp_array = [amount + 2] * (amount + 1)
+def makeChange(coins, total):
+    dp_array = [total + 2] * (total + 1)
     dp_array[0] = 0
     for coin in coins:
         for index in range(len(dp_array)):
             value = dp_array[index]
             if index >= coin:
                 dp_array[index] = min(value, dp_array[index - coin] + 1)
-    if dp_array[amount] == amount + 2:
+    if dp_array[total] == total + 2:
         return -1
-    return dp_array[amount]
+    return dp_array[total]
